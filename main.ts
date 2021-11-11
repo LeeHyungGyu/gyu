5c87594398eedcf8da5c9e70c8e2fc7883ecea1a
LeeHyungGyu <79300684+LeeHyungGyu@users.noreply.github.com>
2021-11-11 16:10:27 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    soundExpression.giggle.playUntilDone()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Square)
+    soundExpression.twinkle.playUntilDone()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.SmallHeart)
+    soundExpression.soaring.playUntilDone()
+})
 basic.forever(function () {
 	
 })
